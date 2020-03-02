5dd7e378ff80f2236bc8de3c2ef650aff385615e
Dandy <2436737939@qq.com>
2020-03-02 10:13:46 +0800

    config

---
+++ src/main/java/com/dandy/CsoitsApplication.java
@@ -0,0 +1,13 @@
+package com.dandy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsoitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsoitsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dandy/CsoitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dandy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsoitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
