57e6249a48b8a8f287d0c6cc21e896d22ab993e2
Dandy <2436737939@qq.com>
2020-06-27 20:25:01 +0800

    add mysql

--- src/test/java/com/dandy/CsoitsApplicationTests.java
+++ src/test/java/com/dandy/CsoitsApplicationTests.java
@@ -5,9 +5,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CsoitsApplicationTests {
+    public static void main(String[] args) {
+
+    }
 
     @Test
     void contextLoads() {
+
     }
 
 }
